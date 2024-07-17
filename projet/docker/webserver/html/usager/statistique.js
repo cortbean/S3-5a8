92aeb52aa0028875415b7c6548f7143a367ad22b
bloulck4 <147091353+blacklist-abi@users.noreply.github.com>
2024-07-17 11:26:50 -0400

    keycloackprob

--- projet/docker/webserver/html/usager/statistique.js
+++ projet/docker/webserver/html/usager/statistique.js
@@ -1,6 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document loaded');
-    fetch('/api/stats/produits-faculte')
+    let keycloak;
+    function initKeycloak() {
+        keycloak = new Keycloak({
+            "realm": "usager",
+            "auth-server-url": "http://localhost:8180/",
+            "ssl-required": "external",
+            "clientId": "frontend",
+            "public-client": true,
+            "confidential-port": 0
+        });
+        keycloak.init({ onLoad: 'login-required' }).then(function (authenticated) {
+            console.log(authenticated ? 'authenticated' : 'not authenticated');
+            if (authenticated) {
+                if (keycloak.hasRealmRole('admin')) {
+                    requestadmin();
+                    document.getElementById('user-role').textContent = 'Admin';
+                } else if (keycloak.hasRealmRole('client')) {
+                    window.location.href = 'index.html'; // Redirige vers la page client
+                }
+                updateCategorie();
+            }
+        }).catch(function (error) {
+            console.error('Échec de l\'initialisation de Keycloak', error);
+        });
+    }
+
+    axios.get('http://localhost:8888/api/stats/produits-faculte')
         .then(response => {
             console.log('API response received');
             return response.json();
